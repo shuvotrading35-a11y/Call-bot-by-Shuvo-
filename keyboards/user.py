c8cb2bed1072f41801c25a48e9003635a6df2971
shuvotrading35-a11y <shuvotrading35@gmail.com>
2026-06-14 00:53:06 +0600

    Update user.py

--- keyboards/user.py
+++ keyboards/user.py
@@ -4,22 +4,28 @@ def user_main_menu(is_admin: bool = False):
     keyboard = [
         [{"text": "🚀 Send Call", "style": "danger"},       {"text": "👤 My Profile",      "style": "primary"}],
         [{"text": "👥 Referral", "style": "primary"},        {"text": "🛒 Buy Subscription", "style": "success"}],
-        [{"text": "🎁 Redeem Code", "style": "success"},  	 {"text": "🎯 Daily Claim",      "style": "success"}],
-        [{"text": "📊 Statistics", "style": "primary"},      {"text": "☎️ Support", "style": "danger"}],
-       
+        [{"text": "🎁 Redeem Code", "style": "success"},     {"text": "🎯 Daily Claim",      "style": "success"}],
+        [{"text": "📊 Statistics", "style": "primary"},      {"text": "☎️ Support",          "style": "danger"}],
     ]
     if is_admin:
         keyboard.append([{"text": "👑 Admin Panel", "style": "danger"}])
-    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True, is_persistent=True)
+    return ReplyKeyboardMarkup(
+        keyboard,
+        resize_keyboard=True,
+        is_persistent=True,
+        input_field_placeholder="⚡ BY SHUVO......"
+    )
 
 def cancel_keyboard():
     return ReplyKeyboardMarkup(
         [[{"text": "🔙 Cancel", "style": "danger"}]],
-        resize_keyboard=True
+        resize_keyboard=True,
+        input_field_placeholder="⚡ BY SHUVO......"
     )
 
 def back_to_user_menu():
     return ReplyKeyboardMarkup(
         [[{"text": "🔙 Main Menu", "style": "success"}]],
-        resize_keyboard=True
+        resize_keyboard=True,
+        input_field_placeholder="⚡ BY SHUVO......"
     )
